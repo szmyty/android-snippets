bbe75977741291e1812aa790619acc7e84f79ffe
Szmyty <szmyty@gmail.com>
2023-07-06 17:01:20 -0400

    Updating gradle.

--- app/src/main/java/com/github/szmyty/android/snippets/MainActivity.kt
+++ app/src/main/java/com/github/szmyty/android/snippets/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.szmyty.android.snippets.ui.theme.AndroidsnippetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +16,9 @@ class MainActivity : ComponentActivity() {
             AndroidsnippetsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidsnippetsTheme {
-        Greeting("Android")
     }
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,41 @@
+@file:Suppress("UnstableApiUsage")
+
+// https://docs.gradle.org/current/userguide/build_cache.html
+buildCache {
+    local {
+        directory = File(rootDir, ".cache")
+        removeUnusedEntriesAfterDays = 7
+    }
+}
+
+pluginManagement {
+    //includeBuild("build-logic")
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+
+            // https://github.com/ben-manes/gradle-versions-plugin/issues/541#issuecomment-957483372
+            content {
+                excludeGroupByRegex("com/github/ben-manes/*")
+            }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "android-snippets"
+include(":app")
